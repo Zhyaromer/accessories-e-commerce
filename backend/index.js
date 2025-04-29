c6647693b3ef0294e9b4ee3d6650e36a70b50d29
Zhyaromer <zhyaraland123@gmail.com>
2025-04-30 00:24:34 +0300

    hosting

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ app.use(session({
 
 app.use(cors({ 
     origin: process.env.NODE_ENV === 'production' 
-    ? 'https://dazhir.netlify.app/' 
+    ? 'https://cool-smakager-2d558a.netlify.app/' 
     : 'http://localhost:5173',
     credentials: true 
 }));
